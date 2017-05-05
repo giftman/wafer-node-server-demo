d3d816d0354d70423716cf4530c6c266e6434619
chufan <121551591@qq.com>
2017-05-05 11:58:54 +0800

    add request

--- routes/location.js
+++ routes/location.js
@@ -1,19 +1,21 @@
 'use strict';
+var request = require('request')
 
-const LoginService = require('qcloud-weapp-server-sdk').LoginService;
+// const LoginService = require('qcloud-weapp-server-sdk').LoginService;
+API_URL = 'http://apis.map.qq.com/ws/geocoder/v1/?key=D2MBZ-GRNRU-WFFVT-4XKS7-GWQ55-RPFMJ&location='
 
 module.exports = (req, res) => {
-    const loginService = LoginService.create(req, res);
-
-    loginService.check()
-        .then(data => {
-            
-            res.json({
-                'code': 0,
-                'message': 'ok',
-                'data': {
-                    'userInfo': data.userInfo,
-                },
-            });
-        });
+    console.log(req.params)
+    request(API_URL + req.params.q, function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+    });
+    res.json({
+        'code': 0,
+        'message': 'ok',
+        'data': {
+            'userInfo': data.userInfo,
+        },
+    });
 };
