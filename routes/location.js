a3561fba0a19acb42444e2e879cc8fccf5b7e7f2
chufan <121551591@qq.com>
2017-05-05 12:11:32 +0800

    query

--- routes/location.js
+++ routes/location.js
@@ -5,8 +5,8 @@ var request = require('request')
 var API_URL = 'http://apis.map.qq.com/ws/geocoder/v1/?key=D2MBZ-GRNRU-WFFVT-4XKS7-GWQ55-RPFMJ&location='
 
 module.exports = (req, res) => {
-    console.log(req.params)
-    request(API_URL + req.params.q, function (error, response, body) {
+    console.log(req.query)
+    request(API_URL + req.query.q, function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         console.log('body:', body); // Print the HTML for the Google homepage.
